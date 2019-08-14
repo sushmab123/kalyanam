0d79826bd78b0728fca833fdefe21496d83628a8
user1 <anuradha.krishnanov7@gmail.com>
2019-08-14 18:04:42 +0530

    test case for impl

---
+++ src/test/java/com/ing/matrimony/service/InvitationServiceImplTest.java
@@ -0,0 +1,48 @@
+package com.ing.matrimony.service;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.ing.matrimony.dto.InvitationDto;
+import com.ing.matrimony.dto.InvitationRequestDto;
+import com.ing.matrimony.entity.Invitation;
+import com.ing.matrimony.repository.InvitationRepository;
+
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class InvitationServiceImplTest {
+
+	
+	@Mock
+	InvitationRepository invitationRepository;
+	
+	@InjectMocks
+	InvitationServiceImpl invitationServiceImpl;
+	
+	
+	@Test
+	public void testInvite() {
+		
+		Invitation invitation=new Invitation();
+
+		invitation.setReceiverId(1L);
+		invitation.setSenderID(2L);
+		invitation.setInvitationStatus("PENDING");
+		InvitationRequestDto InvitationRequestDto=new InvitationRequestDto();
+		InvitationRequestDto.setRecieverId(1L);
+		InvitationRequestDto.setSenderId(2L);
+	when(invitationRepository.save(Mockito.any())).thenReturn(invitation);
+	InvitationDto InvitationDto=invitationServiceImpl.invite(InvitationRequestDto);
+	
+	assertEquals("invitation send", InvitationDto.getMessage());
+	}
+
+}
--- src/test/java/com/ing/matrimony/service/SearchServiceImplTest.java
+++ src/test/java/com/ing/matrimony/service/SearchServiceImplTest.java
@@ -1,14 +1,59 @@
 package com.ing.matrimony.service;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.ing.matrimony.entity.User;
+import com.ing.matrimony.repository.ProfileRepository;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
 public class SearchServiceImplTest {
 
+	@Mock
+	ProfileRepository profileRepositoryMock;
+	
+	@InjectMocks
+	SearchServiceImpl searchServiceImplMock;
+	
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void searchTest() {
+		
+		List<User> userList=new ArrayList<>();
+		User user=new User();
+		user.setAddress("BTM");
+		user.setAge(23);
+		user.setDateOfBirth(LocalDate.now());
+		user.setEmail("a@a.com");
+		user.setFirstName("shashank");
+		user.setGender("Male");
+		user.setLastName("kumar");
+		user.setMaritalStatus("single");
+		user.setMembershipType("classic");
+		user.setMobileNo("8970297757");
+		user.setMotherTongue("hindi");
+		user.setPassword("ok");
+		user.setUserId(1L);
+		userList.add(user);
+		
+		when(profileRepositoryMock.findByAgeRangeAndGender(Mockito.anyInt(), Mockito.anyInt(), Mockito.any())).thenReturn(userList);
+		
+		List<User> userListActual=searchServiceImplMock.search(20, 27, "male");
+		
+        assertEquals(1, userListActual.size());
+	
 	}
 
 }
